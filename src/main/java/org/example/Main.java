6de7ad4bc765d6d409db34f5a0fb713c12b8a2e2
Roman Cherkhavskyi <romko-chera@ukr.net>
2023-03-01 17:01:59 +0200

    JavaDev module 1 project with maven

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import com.google.gson.Gson;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("Roman");
+        person.setLastName("Cherkhavskyi");
+
+        Gson gson = new Gson();
+        System.out.println(gson.toJson(person));
+
+    }
+}
